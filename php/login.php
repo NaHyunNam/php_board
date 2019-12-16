fccb870df4a4bca9bae20b2bb2cb247525eeaaae
NaHyunNam <namnahyundev@gmail.com>
2019-12-16 17:47:11 +0900

    로그인 페이지 디자인

--- php/login.php
+++ php/login.php
@@ -37,38 +37,32 @@
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">로그인을 해주세요</h1>
                   </div>
                   <form class="user">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="아이디를 입력해주세요.">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="비밀번호를 입력해주세요">
                     </div>
                     <a href="index.php" class="btn btn-primary btn-user btn-block">
                       Login
                     </a>
                     <hr>
                     <a href="index.php" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
+                      <i class="fab fa-google fa-fw"></i> 구글 아이디로 로그인하기
                     </a>
                     <a href="index.php" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                      <i class="fab fa-facebook-f fa-fw"></i> 페이스북 아이디로 로그인하기
                     </a>
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="forgot-password.php">Forgot Password?</a>
+                    <a class="small" href="forgot-password.php">아이디, 비밀번호 찾기</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="register.php">Create an Account!</a>
+                    <a class="small" href="register.php">회원가입</a>
                   </div>
                 </div>
               </div>
